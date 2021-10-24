b1f22f4c53e96f8c1271e80d6e063b29f1ca950f
nahkos <sokhan.prum78@gmail.com>
2021-10-24 16:35:42 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.802d96039ca0ec5841f0ae1baf3c604a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd98e8c9891213c0ecb6",
+    "url": "/nahkos/COMP3104/static/css/main.1c5d0057.chunk.css"
+  },
+  {
+    "revision": "cd98e8c9891213c0ecb6",
+    "url": "/nahkos/COMP3104/static/js/main.cd98e8c9.chunk.js"
+  },
+  {
+    "revision": "c29b3d78654a36f56d7e",
+    "url": "/nahkos/COMP3104/static/js/runtime~main.c29b3d78.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/nahkos/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "807b64657aa64f62357dc9ad19f22f4c",
+    "url": "/nahkos/COMP3104/index.html"
+  }
+];
